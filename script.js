d2c1bc26950f47aeadcea04965473e740c2b6fa4
Mayukh Ghosh <mayukhg@gmail.com>
2019-03-22 15:42:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ function deposit_wu(DepositedWithWu){
   	if(listing.state=='ReadyForDeposit'){
     	listing.state='DepositedWithWu';
     }
-  	else if(listing.state=='HARVESTED'){
-    	throw new window.alert('Vegetable Already Harvested');
+  	else if(listing.state=='DepositedWithWu'){
+    	throw new window.alert('Amount already deposited');
     }
-  else if(listing.state=='DELIVERED_W'){
-  	throw new window.alert('Vegetable Delivered at the warehouse');
+  else if(listing.state=='SentToAggregator'){
+  	throw new window.alert('Amount already sent to aggregator');
   }
-  else if(listing.state=='DELIVERED_C'){
-  	throw new window.alert('Vegetable Delivered to the consumer');
+  else if(listing.state=='ReceivedByAggregator'){
+  	throw new window.alert('Amount already received by aggregator');
   }
   	else{
-    	throw new window.alert('Out For Delivery');
+    	throw new window.alert('Not a valid transaction');
     }
   return getAssetRegistry('org.acme.model.supplychain.TransStatus')
         .then(function(transstatusListingRegistry) {
-            // save the vegetable listing
+            // save the Transaction Status listing
             return transstatusListingRegistry.update(listing);
         });
 }
