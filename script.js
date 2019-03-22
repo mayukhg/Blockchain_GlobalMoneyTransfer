b5effba15750ede2dddaa0933701f7311e438aa7
mayukhg <mayukhg@gmail.com>
2019-03-22 15:00:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,40 @@
-/**
-      * @param {org.acme.model.supplychain.SentToAggregator} makeOrderw
-       * @transaction 
- */
-function makeOrderw(SentToAggregator) 
-{
-    var listing = SentToAggregator.listing;
-    if (listing.state == 'HARVEST') {
-        throw new window.alert('Vegetable not ready for delivery !!');
-    }
-  	else if(listing.state=='DepositedWithWu'){
-    	listing.state='SentToAggregator';
-    }
-  	else if(listing.state=='IN_DELIVER_W'){
-    	throw new window.alert('Arriving soon at warehouse !!');
-    }
-  else if(listing.state=='DELIVERED_W'){
-  		throw new window.alert('Already delivered at warehouse');
-  }
-    
-   return getAssetRegistry('org.acme.model.supplychain.VegetableListing')
-        .then(function(vegetableListingRegistry) {
-            // save the vegetable listing
-            return vegetableListingRegistry.update(listing);
-        });
-   
-}
-
-/**
-      * @param {org.acme.model.supplychain.Orderc} makeOrderc
-       * @transaction 
- */
-function makeOrderc(Orderc) 
-{
-    var listing = Orderc.listing;
-  	if(listing.state=='IN_DELIVER_C'){
-    	throw new window.alert('About to deliver to consumer !!');
-    }
-  
-  	else if(listing.state=='DELIVERED_W' ){
-   		listing.state='IN_DELIVER_C';
-    }
-  else{
-  	throw new window.alert('Out of Stock !!');
-  }
-    
-   return getAssetRegistry('org.acme.model.supplychain.VegetableListing')
-        .then(function(vegetableListingRegistry) {
-            // save the vegetable listing
-            return vegetableListingRegistry.update(listing);
-        });
-}
 
 /**
       * @param {org.acme.model.supplychain.DepositedWithWu} harvest
        * @transaction 
  */
-function harvest(DepositedWithWu){
+function deposit_wu(DepositedWithWu){
 	var listing=DepositedWithWu.listing;
   	if(listing.state=='ReadyForDeposit'){
     	listing.state='DepositedWithWu';
+    }
+  	else if(listing.state=='HARVESTED'){
+    	throw new window.alert('Vegetable Already Harvested');
+    }
+  else if(listing.state=='DELIVERED_W'){
+  	throw new window.alert('Vegetable Delivered at the warehouse');
+  }
+  else if(listing.state=='DELIVERED_C'){
+  	throw new window.alert('Vegetable Delivered to the consumer');
+  }
+  	else{
+    	throw new window.alert('Out For Delivery');
+    }
+  return getAssetRegistry('org.acme.model.supplychain.VegetableListing')
+        .then(function(vegetableListingRegistry) {
+            // save the vegetable listing
+            return vegetableListingRegistry.update(listing);
+        });
+}
+
+/**
+      * @param {org.acme.model.supplychain.SentToAggregator} harvest
+       * @transaction 
+ */
+function sentto_aggr(SentToAggregator){
+	var listing=SentToAggregator.listing;
+  	if(listing.state=='DepositedWithWu'){
+    	listing.state='SentToAggregator';
     }
   	else if(listing.state=='HARVESTED'){
     	throw new window.alert('Vegetable Already Harvested');
@@ -84,7 +60,7 @@ function harvest(DepositedWithWu){
        * @transaction 
  */
 
-function Deliver_w(ReceivedByAggregator){
+function receive_aggr(ReceivedByAggregator){
   var listing=ReceivedByAggregator.listing;
 
   if(listing.state=='SentToAggregator'){
@@ -111,7 +87,7 @@ function Deliver_w(ReceivedByAggregator){
        * @transaction 
  */
 
-function Deliver_c(SentToBank){
+function sentto_bank(SentToBank){
   var listing=SentToBank.listing;
   if(listing.state=='ReceivedByAggregator'){
     listing.state='SentToBank';
@@ -137,7 +113,7 @@ function Deliver_c(SentToBank){
        * @transaction 
  */
 
-function Deliver_c(ReceivedByBank){
+function receive_bank(ReceivedByBank){
   var listing=ReceivedByBank.listing;
   if(listing.state=='SentToBank'){
     listing.state='ReceivedByBank';
@@ -163,7 +139,7 @@ function Deliver_c(ReceivedByBank){
        * @transaction 
  */
 
-function Deliver_c(CreditedToReceipient){
+function credit_recipient(CreditedToReceipient){
   var listing=CreditedToReceipient.listing;
   if(listing.state=='ReceivedByBank'){
     listing.state='CreditedToReceipient';
@@ -189,7 +165,7 @@ function Deliver_c(CreditedToReceipient){
        * @transaction 
  */
 
-function Deliver_c(ConfirmedByReceipient){
+function conf_recipient(ConfirmedByReceipient){
   var listing=ConfirmedByReceipient.listing;
   if(listing.state=='CreditedToReceipient'){
     listing.state='ConfirmedByReceipient';
